4d53321db1f1f100eea867786e07986e8d3a0e14
ha11lucin8 <214943980+ha11ucin8@users.noreply.github.com>
2026-03-22 20:31:45 -0400

    update qwen2 tok config

--- easyeditor/editors/editor.py
+++ easyeditor/editors/editor.py
@@ -128,7 +128,7 @@ class BaseEditor:
                 self.tok.pad_token_id = self.tok.eos_token_id
             elif 'qwen2' in self.model_name.lower():
                 self.model = AutoModelForCausalLM.from_pretrained(self.model_name,trust_remote_code=True, torch_dtype=torch_dtype if hparams.alg_name not in ['MEND'] else torch.bfloat16, device_map=device_map)
-                self.tok = AutoTokenizer.from_pretrained(self.model_name, eos_token='<|endoftext|>', pad_token='<|endoftext|>',unk_token='<|endoftext|>', trust_remote_code=True)
+                self.tok = AutoTokenizer.from_pretrained(self.model_name, trust_remote_code=True)
                 self.tok.pad_token_id = self.tok.eos_token_id
             elif 'qwen' in self.model_name.lower():
                 self.model = AutoModelForCausalLM.from_pretrained(self.model_name,fp32=False,trust_remote_code=True, **model_kwargs)
